2ee67b76a887a98be4ed698ed17726a8ad30c19b
yiying <zhutt0607@foxmail.com>
2024-02-22 21:26:04 +0800

    新增上传文件功能

--- flaskProject/app/views/__init__.py
+++ flaskProject/app/views/__init__.py
@@ -1,12 +1,14 @@
 from .book import book_view
 from .register import register_view
 from .login import login_view
+from .fileUpload import fileUpload_view
 from flask import Blueprint
 
 bp = Blueprint('api', __name__)
 
 bp.add_url_rule('/books', defaults={'book_id': None}, view_func=book_view, methods=['GET'])
 bp.add_url_rule('/books', view_func=book_view, methods=['POST'])
+bp.add_url_rule('/file', view_func=fileUpload_view, methods=['POST'])
 bp.add_url_rule('/register', view_func=register_view, methods=['POST'])
 bp.add_url_rule('/login', view_func=login_view, methods=['POST'])
 bp.add_url_rule('/books/<int:book_id>', view_func=book_view, methods=['GET', 'PUT', 'DELETE'])
---
+++ flaskProject/app/views/fileUpload.py
@@ -0,0 +1,47 @@
+import os
+from flask import request
+from flask.views import MethodView
+
+
+class FileUploadApi(MethodView):
+    def post(self):
+        # 检查是否有文件上传
+        if 'file' not in request.files:
+            return {
+                'status': 'error',
+                'message': 'No file provided',
+                'code': 1
+            }
+
+        # 获取上传的文件
+        file = request.files['file']
+
+        # 检查文件是否存在
+        if file.filename == '':
+            return {
+                'status': 'error',
+                'message': 'No file selected',
+                'code': 2
+            }
+
+        # 指定相对路径保存文件，相对于当前工作目录
+        upload_folder = os.path.join('..', 'file')
+
+        # 获取当前工作目录
+        current_directory = os.getcwd()
+
+        # 构建保存文件的完整路径
+        save_path = os.path.join(current_directory, upload_folder, file.filename)
+
+        # 保存文件
+        file.save(save_path)
+
+        # 返回保存成功的消息
+        return {
+            'status': 'success',
+            'message': 'File saved successfully',
+            'code': 0
+        }
+
+
+fileUpload_view = FileUploadApi.as_view('fileUpload_api')
